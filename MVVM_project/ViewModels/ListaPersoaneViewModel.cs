a4d7f4a5e2c22f97e32adfb500aa5dfb45ab5bdd
AndreiPitt <andreideaconu288@gmail.com>
2026-02-27 13:47:31 +0200

    I started to learn about MVVM Arhitecture in WPF

---
+++ MVVM_project/ViewModels/ListaPersoaneViewModel.cs
@@ -0,0 +1,52 @@
+﻿using MVVM_project.Commands;
+using MVVM_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace MVVM_project.ViewModels
+{
+    class ListaPersoaneViewModel : INotifyPropertyChanged
+    {
+        private PersoanaViewModel _persoanaSelectata;
+        public ObservableCollection<PersoanaViewModel> Persoane { get; }
+
+        public PersoanaViewModel PersoanaSelectata
+        {
+            get => _persoanaSelectata;
+            set
+            {
+                _persoanaSelectata = value;
+                OnPropertyChanged(nameof(PersoanaSelectata));
+            }
+        }
+
+        public ListaPersoaneViewModel() 
+        {
+            PersoanaSelectata = new PersoanaViewModel (new Persoana { Name = "Pitt", Age = 20 });
+            Persoane = new ObservableCollection<PersoanaViewModel>()
+            {
+                PersoanaSelectata,
+                new PersoanaViewModel ( new Persoana { Name = "Mihai", Age = 23 } ),
+                new PersoanaViewModel ( new Persoana { Name = "Andrei", Age = 23 } )
+            };
+
+            AdaugaPersoanaCommand = new RelayCommand(o =>
+            {
+                Persoane.Add(new PersoanaViewModel(new Persoana {Name = "Persoana noua", Age = 20} ));
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public ICommand AdaugaPersoanaCommand { get; }
+    }
+}
---
+++ MVVM_project/ViewModels/PersoanaViewModel.cs
@@ -0,0 +1,76 @@
+﻿using MVVM_project.Commands;
+using MVVM_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace MVVM_project.ViewModels
+{
+    class PersoanaViewModel : INotifyPropertyChanged
+    {
+        private Persoana _persoana;
+        
+        public Persoana Persoana
+        {
+            get => _persoana; 
+            set 
+            {
+                _persoana = value;
+                OnPropertyChanged(nameof(Persoana));
+            }
+        }
+
+        public string Name
+        {
+            get => _persoana.Name;
+            set
+            {
+                _persoana.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int Age
+        {
+            get => _persoana.Age;
+            set
+            {
+                _persoana.Age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        public PersoanaViewModel(Persoana persoana)
+        {
+            _persoana = persoana;
+
+            SchimbaNumeCommand = new RelayCommand(o =>
+            {
+                Name = "Nume schimbat din ViewModel!";
+            });
+
+            IncrementeazaVarstaCommand = new RelayCommand(o =>
+            {
+                Age += 1;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        public ICommand SchimbaNumeCommand { get; }
+        public ICommand IncrementeazaVarstaCommand { get; }
+
+
+
+    }
+}
