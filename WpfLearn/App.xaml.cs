dc543dbc12e6dc591125cecbc5569ae7db1741c0
AndreiPitt <andreideaconu288@gmail.com>
2026-02-27 07:39:39 +0200

    try to learn MVVM

--- WpfLearn/App.xaml.cs
+++ WpfLearn/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using WpfLearn.ViewModel;
 
 namespace WpfLearn
 {
@@ -9,6 +10,14 @@ namespace WpfLearn
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            WpfLearn.MainWindow window = new MainWindow();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
     }
 
 }
