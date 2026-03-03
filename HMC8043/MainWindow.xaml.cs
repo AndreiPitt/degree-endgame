ae44e2eccd6f1acf4a8148d9715140598946a009
Pitt <gf0sgaming18@gmail.com>
2026-03-03 13:56:10 +0200

    Am facut un program de antrenament pentru a controla o sursa HMC8043

---
+++ HMC8043/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Ivi.Visa;
+
+namespace HMC8043
+{
+    public partial class MainWindow : Window
+    {
+        private IMessageBasedSession _session;
+        private bool _isOutputOn = false;
+        private string _canal = "1"; // Valoarea implicita
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string resourceName = TxtDeviceId.Text;
+
+                // Deschidem conexiunea
+                _session = GlobalResourceManager.Open(resourceName) as IMessageBasedSession;
+
+                _session.FormattedIO.WriteLine("*IDN?");
+                string idnResponse = _session.FormattedIO.ReadLine();
+
+                TxtStatus.Text = "Conectat:\n" + idnResponse;
+                TxtStatus.Foreground = Brushes.Green;
+            }
+            catch (Exception ex)
+            {
+                TxtStatus.Text = "Eroare conectare:\n" + ex.Message;
+                TxtStatus.Foreground = Brushes.Red;
+
+                if (_session != null)
+                {
+                    _session.Dispose();
+                    _session = null;
+                }
+            }
+        }
+
+        private void BtnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (RbCh1.IsChecked == true) _canal = "1";
+            else if (RbCh2.IsChecked == true) _canal = "2";
+            else if (RbCh3.IsChecked == true) _canal = "3";
+            _session.FormattedIO.WriteLine($"INST OUT{_canal}");
+            string tensiune = TxtVoltage.Text.Replace(',', '.');
+            string curent = TxtCurrent.Text.Replace(',', '.');
+
+            _session.FormattedIO.WriteLine($"VOLT {tensiune}");
+            _session.FormattedIO.WriteLine($"CURR {curent}");
+            MessageBox.Show($"Setări aplicate cu succes pe CH{_canal} ({tensiune}V, {curent}A)");
+
+        }
+
+        private void BtnOutput_Click(object sender, RoutedEventArgs e)
+        {
+            _isOutputOn = !_isOutputOn;
+            if (RbCh1.IsChecked == true) _canal = "1";
+            else if (RbCh2.IsChecked == true) _canal = "2";
+            else if (RbCh3.IsChecked == true) _canal = "3";
+
+            _session.FormattedIO.WriteLine($"INST OUT{_canal}");
+
+            _session.FormattedIO.WriteLine("OUTP?");
+            string stareaCurenta = _session.FormattedIO.ReadLine().Trim(); // .Trim() pentru a elimina spatiile 
+
+            if (stareaCurenta == "0") 
+            {
+                _session.FormattedIO.WriteLine("OUTP 1"); 
+
+                BtnOutput.Background = System.Windows.Media.Brushes.LightGreen;
+                BtnOutput.Content = $"CH{_canal} ON";
+                MessageBox.Show($"Canalul {_canal} a fost ACTIVAT");
+            }
+            else 
+            {
+                _session.FormattedIO.WriteLine("OUTP 0"); 
+
+                BtnOutput.Background = System.Windows.Media.Brushes.LightCoral;
+                BtnOutput.Content = $"CH{_canal} OFF";
+                MessageBox.Show($"Canalul {_canal} a fost DEZACTIVAT");
+                
+            }
+        }
+    }
+}
---
+++ SursaHMC8043/Program.cs
@@ -0,0 +1,43 @@
+﻿using Ivi.Visa;
+using System;
+
+
+namespace SursaHMC8043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. ADRESA (Se NI MAX)
+            string adresaInstrument = "USB0::0x0AAD::0x0135::034318994::INSTR";
+
+            try
+            {
+                Console.WriteLine("Încercăm conectarea cu sursa...");
+
+                using (var session = GlobalResourceManager.Open(adresaInstrument) as IMessageBasedSession)
+                {
+                    Console.WriteLine("Conectat cu succes!\n");
+
+                    // (Comanda SCPI)
+                    string comanda = "*IDN?";
+                    double voltage = 5.0;
+                    double current = 0.5;
+
+                    Console.WriteLine($"Trimitem comanda: {comanda}");
+                    session.FormattedIO.WriteLine(comanda);
+            
+
+                    string raspuns = session.FormattedIO.ReadLine();
+
+                    Console.WriteLine($"\nSursa a răspuns: {raspuns}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nUps, a apărut o problemă: {ex.Message}");
+            }
+
+        }
+    }
+}
